9f10bfa4dc0dc85365b264223a8fe6013c5801d5
Markus Zinggl <markus.zinggl@student.tugraz.at>
2021-04-27 15:11:05 +0200

    VP-001 Handled clicks on the SignUp link [ZMarkus, SP]

--- App/app/src/main/java/at/tugraz/vaccinationpassport/MainActivity.kt
+++ App/app/src/main/java/at/tugraz/vaccinationpassport/MainActivity.kt
@@ -2,11 +2,23 @@ package at.tugraz.vaccinationpassport
 
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onRegisterClicked(view: View) {
+        displayLoginError("Registering is not Implemented")
+    }
+
+    private fun displayLoginError(message: String) {
+        val errorView = findViewById<TextView>(R.id.errorMessageView)
+        errorView.text = message
+        errorView.visibility = View.VISIBLE
     }
 }
