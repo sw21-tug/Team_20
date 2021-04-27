705f5e87f2fdd2349a4d12707b8652ecae8aa66f
Markus Zinggl <markus.zinggl@student.tugraz.at>
2021-04-27 15:37:39 +0200

    VP-001 Passport number and password are retrieved once the Login-button is clicked [ZMarkus, SP]

--- App/app/src/main/java/at/tugraz/vaccinationpassport/MainActivity.kt
+++ App/app/src/main/java/at/tugraz/vaccinationpassport/MainActivity.kt
@@ -3,13 +3,28 @@ package at.tugraz.vaccinationpassport
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
+import at.tugraz.vaccinationpassport.backend.Server
+import at.tugraz.vaccinationpassport.backend.api.data.LoginDetails
 
 class MainActivity : AppCompatActivity() {
+    private val server = Server()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onLoginClicked(view: View) {
+        val loginDetails = getEnteredData()
+        if (loginDetails == null) {
+            displayLoginError("Invalid Passport Number / Password")
+            return
+        }
+        server.login(loginDetails)
     }
 
     fun onRegisterClicked(view: View) {
@@ -20,5 +35,19 @@ class MainActivity : AppCompatActivity() {
         val errorView = findViewById<TextView>(R.id.errorMessageView)
         errorView.text = message
         errorView.visibility = View.VISIBLE
+    }
+
+    private fun getEnteredData(): LoginDetails? {
+        val passport = findViewById<EditText>(R.id.loginPassportNumber) ?: return null
+        val passportText = passport.text
+        if (passportText.isEmpty() || !passportText.isDigitsOnly()) {
+            return null
+        }
+        val passportNumber = passportText.toString()
+
+        val password = findViewById<EditText>(R.id.loginPassword) ?: return null
+        val passwordString = password.text.toString()
+
+        return LoginDetails(passportNumber, passwordString)
     }
 }
---
+++ App/app/src/main/java/at/tugraz/vaccinationpassport/backend/Server.kt
@@ -0,0 +1,13 @@
+package at.tugraz.vaccinationpassport.backend
+
+
+import at.tugraz.vaccinationpassport.backend.api.data.LoginDetails
+
+
+class Server() {
+    private lateinit var authToken: String
+
+    fun login(loginDetails: LoginDetails) {
+
+    }
+}
