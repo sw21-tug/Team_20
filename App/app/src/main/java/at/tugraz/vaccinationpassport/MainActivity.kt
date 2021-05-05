30c115934df70a632b40ac57128e950b6b1e26b7
Irena Ruprecht <irena.ruprecht@gmail.com>
2021-05-05 09:14:06 +0200

    VP-011-A Add functionality to open and close hamburger menu, display about and language button [RI, ZMarkus]

--- App/app/src/main/java/at/tugraz/vaccinationpassport/MainActivity.kt
+++ App/app/src/main/java/at/tugraz/vaccinationpassport/MainActivity.kt
@@ -2,11 +2,39 @@ package at.tugraz.vaccinationpassport
 
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle : ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val drawerLayout = findViewById(R.id.drawer_layout) as DrawerLayout
+        // set the toolbar as action bar
+        setSupportActionBar(toolbar)
+
+        toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+
+        toggle.isDrawerIndicatorEnabled = true
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem) : Boolean{
+        if(toggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
