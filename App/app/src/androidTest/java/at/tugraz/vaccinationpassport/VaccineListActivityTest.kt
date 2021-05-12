aa325bc61f214739eeda3dbca5f26935e611cf99
Nicholas Gratzer <n.d.gratzer@student.tugraz.at>
2021-05-12 11:24:55 +0200

    VP-004-D add test for profile information in VaccineListActivity [GN,RI]

--- App/app/src/androidTest/java/at/tugraz/vaccinationpassport/VaccineListActivityTest.kt
+++ App/app/src/androidTest/java/at/tugraz/vaccinationpassport/VaccineListActivityTest.kt
@@ -12,6 +12,71 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class VaccineListActivityTest
 {
+    private fun tvTest(id:Int, text:String){
+        val scenario = ActivityScenario.launch(VaccineListActivity::class.java)
+        onView(withId(id)).check(matches(isDisplayed()))
+
+        onView(withId(id)).check(matches(withText(text)))
+        scenario.close()
+    }
+    @Test
+    fun testUserNameLabel()
+    {
+        /*
+        * Test if Name Label is displayed
+         */
+        val id = R.id.tvNameDisplay
+        val text = "Name:"
+
+        tvTest(id,text)
+    }
+
+    @Test
+    fun testUserAgeLabel()
+    {
+        /*
+        * Test if age Label is displayed
+         */
+        val id = R.id.tvAgeDisplay
+        val text = "Age:"
+
+        tvTest(id,text)
+    }
+
+    @Test
+    fun testUserPassNrLabel()
+    {
+        /*
+        * Test if Passport nr Label is displayed
+         */
+        val id = R.id.tvPassNrDisplay
+        val text = "Passport Nr:"
+
+        tvTest(id,text)
+    }
+
+    @Test
+    fun testUserNrVacLabel()
+    {
+        /*
+        * Test if NrVacciens Label is displayed
+         */
+        val id = R.id.tvNrVacDisplay
+        val text = "Nr of Vaccines:"
+
+        tvTest(id,text)
+    }
+
+    @Test
+    fun testProfilePicText()
+    {
+        /*
+        * Test if profile picture is displayed
+         */
+        val scenario = ActivityScenario.launch(VaccineListActivity::class.java)
+        onView(withId(R.id.imPassportImage)).check(matches(isDisplayed()))
+        scenario.close()
+    }
 
     @Test
     fun testRecyclerViewDisplay()
