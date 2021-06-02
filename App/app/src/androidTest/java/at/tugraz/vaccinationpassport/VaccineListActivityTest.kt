af911cb1fb067f413115eb5a07f2ae2d43605cf9
Florian Michelic <florian.michelic@student.tugraz.at>
2021-06-02 11:20:21 +0200

    VP-004-C implemented test vor vaccine list real data [MF, RI]

--- App/app/src/androidTest/java/at/tugraz/vaccinationpassport/VaccineListActivityTest.kt
+++ App/app/src/androidTest/java/at/tugraz/vaccinationpassport/VaccineListActivityTest.kt
@@ -1,17 +1,21 @@
 package at.tugraz.vaccinationpassport
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import it.xabaras.android.espresso.recyclerviewchildactions.RecyclerViewChildActions.Companion.childOfViewAtPositionWithMatcher
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 class VaccineListActivityTest
 {
-    private fun checkLabel(id:Int, text:String){
+    private fun checkLabel(id: Int, text: String){
         onView(withId(id)).check(matches(isDisplayed()))
         onView(withId(id)).check(matches(withText(text)))
     }
@@ -29,6 +33,45 @@ class VaccineListActivityTest
         checkLabel(R.id.tvDisease, "Disease")
         onView(withId(R.id.imPassportImage)).check(matches(isDisplayed()))
         onView(withId(R.id.rvVaccineList)).check(matches(isDisplayed()))
+
+        scenario.close()
+    }
+
+    @Test
+    fun testVaccineListRealData()
+    {
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.loginPassportNumber)).perform(
+            ViewActions.typeText("12345678"),
+            ViewActions.closeSoftKeyboard()
+        )
+
+        onView(withId(R.id.loginPassword)).perform(
+            ViewActions.typeText("password"),
+            ViewActions.closeSoftKeyboard()
+        )
+
+        onView(withId(R.id.loginButton)).perform(ViewActions.click())
+
+        Thread.sleep(1000)
+
+        onView(withId(R.id.tvInfoList)).perform(ViewActions.click())
+
+        Thread.sleep(250)
+
+        checkLabel(R.id.tvNameText, "Max Mustermann")
+        checkLabel(R.id.tvAgeText, "30")
+        checkLabel(R.id.tvPassNrText, "12345678")
+        checkLabel(R.id.tvNrVacText, "2")
+
+        onView(withId(R.id.rvVaccineList))
+            .check(matches(childOfViewAtPositionWithMatcher(R.id.tvVaccineName,
+                0, withText("Vaccine Nr. 0"))))
+
+        onView(withId(R.id.rvVaccineList))
+                .check(matches(childOfViewAtPositionWithMatcher(R.id.tvVaccinationDate,
+        0, withText("2021-01-01"))))
 
         scenario.close()
     }
